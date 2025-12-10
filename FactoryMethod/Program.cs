ada6da947d8bded291a5c09211aa4b9a85cd1554
LUIS ALEJANDRO BRAVO BELLO <lb2024-1279@uce.edu.do>
2025-12-10 19:55:51 -0400

    Agregar archivos de proyecto.

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public interface Inotificacion
+{
+    void Enviar(string mensaje);
+    
+    
+    
+}
+
+public class NotificacionEmail : Inotificacion
+{
+    public void Enviar(string mensaje)
+    {
+        Console.WriteLine($"Enviando Email : {mensaje} ");
+    }
+}
+
+public class NotificacionSMS : Inotificacion 
+{
+    public void Enviar(string mensaje)
+    {
+        Console.WriteLine($"Enviando SMS : {mensaje} ");
+    }
+}
+
+// fABRICA
+
+public static class NotificacionFactory
+{
+
+    public static Inotificacion Crear(string tipo)
+    {
+       switch (tipo.ToLower())
+       {
+            case "email": return new NotificacionEmail();
+            case "sms": return new NotificacionSMS();
+            default: throw new ArgumentException("Tipo no soportado");
+       }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese tipo de notificacion (email / sms): ");
+        string tipo = Console.ReadLine();
+
+        Inotificacion notificacion = NotificacionFactory.Crear(tipo);
+        notificacion.Enviar("Su mensaje ha sio enviado");
+    }
+}
+
